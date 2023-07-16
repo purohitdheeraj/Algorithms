8634d357d000e538247386b1a045394c2824f4f7
Dheeraj Purohit <purohitdheeraj89@gmail.com>
2023-07-16 07:23:53 +0530

    feat : palindrome

---
+++ isPalindrome.js
@@ -0,0 +1,16 @@
+function isPalindrome(string) {
+	// 2 pointer
+	// left, right
+	// if left not equal right falsewa
+	// left++ right--
+	// true
+	string = string.toLowerCase();
+	let left = 0;
+	let right = string.length - 1;
+	while (left <= right) {
+		if (string[left++] !== string[right--]) return false;
+	}
+	return true;
+}
+
+console.log(isPalindrome("123321"));
